7fffdff5c59dd4352a85511c6c6398f7a2cf1fd3
Chenry <henrychen0214@gmail.com>
2024-08-25 16:19:54 -0400

    asdf

---
+++ HW01.py
@@ -0,0 +1,66 @@
+"""
+Georgia Institute of Technology - CS1301
+Homework 1 - Functions & Expressions
+"""
+
+#########################################
+
+"""
+Function Name: personalizedMessage()
+Parameters: N/A
+Returns: None (NoneType)
+"""
+
+def personalizedMessage():
+    pass
+
+
+#########################################
+
+"""
+Function Name: gradeCalculator()
+Parameters: N/A
+Returns: None (NoneType)
+"""
+
+def gradeCalculator():
+    pass
+
+
+#########################################
+
+"""
+Function Name: lunchTime()
+Parameters: N/A
+Returns: None (NoneType)
+"""
+
+def lunchTime():
+    pass
+
+
+#########################################
+
+"""
+Function Name: snackBudget()
+Parameters: N/A
+Returns: None (NoneType)
+"""
+
+def snackBudget():
+    pass
+
+
+#########################################
+
+"""
+Function Name: freeTime()
+Parameters: N/A
+Returns: None
+"""
+
+def freeTime():
+    pass
+
+
+#########################################
