1abffad3ecf8f34c9560f1b66be236a41ca8aefc
Chenry <henrychen0214@gmail.com>
2024-09-04 17:26:04 -0400

    asasdf

---
+++ HW02.py
@@ -0,0 +1,66 @@
+"""
+Georgia Institute of Technology - CS1301
+Homework 2 - Conditionals
+"""
+
+#########################################
+
+"""
+Function Name: auroraFate()
+Parameters: age (int), isSunset (bool)
+Returns: birthdayFate (str)
+"""
+
+def auroraFate():
+    pass
+
+
+#########################################
+
+"""
+Function Name: identifyObjects()
+Parameters: material (str), weight (float)
+Returns: objectName (str)
+"""
+
+def identifyObjects():
+    pass
+
+
+#########################################
+
+"""
+Function Name: cinderellaSearch()
+Parameters: candidateName (str), shoeSize (int)
+Returns: suitableStr (str)
+"""
+
+def cinderellaSearch():
+    pass
+
+
+#########################################
+
+"""
+Function Name: voyageDecision()
+Parameters: weatherType (str), timeOfDay (str)
+Returns: None (NoneType)
+"""
+
+def voyageDecision():
+    pass
+
+
+#########################################
+
+"""
+Function Name: aWholeNewWorld()
+Parameters: numOfGuards (int), isAsleep (bool)
+Returns: response (str)
+"""
+
+def aWholeNewWorld():
+    pass
+
+
+#########################################
