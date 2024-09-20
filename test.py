6d197430f459342e35f57ef0c0f91e2040627f3e
Chenry <henrychen0214@gmail.com>
2024-09-20 16:07:01 -0400

    asdf

--- test.py
+++ test.py
@@ -1,3 +1,50 @@
 text = "CS1301 is fun"
-print(text[2:12:3])
-print(text[3:7] + text[2:3] * 2) 
+
+
+# def assembleTeam(name):
+#     for num in range(len(name) - 1):
+#         print(name)
+#         if len(name) <= 5:
+#             if len(name) % 2 == 1:
+#                 print("Keeper!")
+#         if len(name) % 2 == 1:
+#             print("Seeker!")
+#         else:
+#             print("Let's win!")
+#             return "I love quidditch"
+# print(assembleTeam("Ron"))
+
+
+# for i in range(len("ron") - 1):
+#     print(i)
+
+# name = "ABCDEFG"
+# name = name.lower()
+# for i in range(len(name)-1,0,-1):
+#     print(name[i])
+
+
+# def hogwartsHello():
+#     print("Welcome!")
+
+# def hogwartsWelcome(name):
+#     if hogwartsHello() == "Welcome!":
+#         print("No sorting hat for you!")
+#     else:
+#         print("thing")
+
+# hogwartsWelcome("asdf")
+
+# print(name[6::-1])
+
+# for i in range(2, 9):
+#     print(i)
+
+# print(not False)
+
+# thing = "0123456"
+print(4%3)
+
+alist = ["cat","dog","bird","cat"]
+
+print("cat" in alist and "c" in alist)
