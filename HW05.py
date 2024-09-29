0f3c40ce4f2afee8e9dff20e688437c9a3a8a3fc
Chenry <henrychen0214@gmail.com>
2024-09-28 22:34:49 -0400

    asdf

--- HW05.py
+++ HW05.py
@@ -34,9 +34,24 @@ Returns: chosenActivities ( list )
 """
 
 def autumnBucketList(a):
-    
+    copy = []
+    for thing in a:
+        if thing[1] == 0:
+            continue
+        else:
+            copy.append(thing)
+    if copy == []:
+        print("Is it summertime yet?")
+        return []
+    copy.sort(key=lambda x:x[1])
+    copy.reverse()
+    out = []
+    for things in copy:
+        out.append(things[0])
+    if copy != []:
+        print(f"With {copy[0][1]} votes, {copy[0][0]} wins!")
+    return out
     pass
-
 
 #########################################
 
@@ -45,10 +60,24 @@ Function Name: applePicker()
 Parameters: schedule ( list )
 Returns: dates ( list )
 """
-
-def applePicker():
+import calendar
+def applePicker(a):
+    out = []
+    months = calendar.month_name
+    for things in a:
+        b = []
+        b = things.split('-')
+        for i in range (len(b)):
+            b[i] = int(b[i])
+        if calendar.isleap(b[2]):
+            continue
+        if calendar.weekday(b[2], b[0], b[1]) == 5 or calendar.weekday(b[2], b[0], b[1]) == 6:
+            out.append(f"{months[b[0]]} {b[1]}, {b[2]}")
+    if out == []:
+        return "Sorry, we cannot pick any apples :("
+    else:
+        return out
     pass
-
 
 #########################################
 
@@ -57,8 +86,17 @@ Function Name: hayrideHero()
 Parameters: coordinateList ( list )
 Returns: distanceStr ( str )
 """
-
-def hayrideHero():
+import math
+def hayrideHero(a):
+    total = 0
+    lastx = 0
+    lasty = 0
+    for things in a:
+        dist = math.sqrt((things[1] - lastx)**2 + (things[2] - lasty)**2)
+        total += dist
+        lastx = things[1]
+        lasty = things[2]
+    return f"This hayride is {round(total, 2)} miles long!"
     pass
 
 
@@ -70,7 +108,21 @@ Parameters: personList ( list )
 Returns: winnerAndLoser ( str )
 """
 
-def jumpKing():
+from leafTally import *
+
+def jumpKing(a):
+    out = []
+    if len(a) < 2:
+        return "Please enter 2 TAs!"
+    else:
+        for things in a:
+            leafletter = []
+            for l in things[1]:
+                leafletter.append(l[0])
+            out.append((things[0], leafTally(leafletter)))
+        out.sort(key=lambda x:x[1])
+        out.reverse()
+        return f"{out[0][0]} wins with {out[0][1]} points! {out[len(out)-1][0]} lost with only {out[len(out)-1][1]} points..."
     pass
 
 
