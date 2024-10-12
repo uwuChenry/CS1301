a0af85c2600e56c177014467068847aa3c44802b
Chen, Po Cheng <pchen432@gatech.edu>
2024-10-11 22:36:18 -0400

    asdf

---
+++ HW07.py
@@ -0,0 +1,78 @@
+"""
+Georgia Institute of Technology - CS1301
+Homework 7 - File I/O and CSV Files
+"""
+
+#########################################
+
+"""
+Function Name: findGreenestFlight()
+Parameters: start (str), end (str)
+Returns: totalCarbonEmissions (float)
+"""
+
+def findGreenestFlight():
+    pass
+
+
+#########################################
+
+"""
+Function Name: lowestEmissions()
+Parameters: flights (dict)
+Returns: None (NoneType)
+"""
+
+def lowestEmissions():
+    pass
+
+
+#########################################
+
+"""
+Function Name: csvToList()
+Parameters: fileName (str)
+Returns: flightsList (list)
+"""
+
+def csvToList():
+    pass
+
+
+#########################################
+
+"""
+Function Name: csvToDict()
+Parameters: fileName (str)
+Returns: flightsDict (dict)
+"""
+
+def csvToDict():
+    pass
+
+
+#########################################
+
+"""
+Function Name: worstRoutes_list()
+Parameters: originCountry (str), destCountry (str), flightsList (list)
+Returns: message (str)
+"""
+
+def worstRoutes_list():
+    pass
+
+
+#########################################
+
+"""
+Function Name: worstRoutes_dict()
+Parameters: originCountry (str), destCountry (str), flightsDict (dict)
+Returns: message (str)
+"""
+
+def worstRoutes_dict():
+    pass
+
+
+#########################################
--- test.py
+++ test.py
@@ -1,36 +1,5 @@
-import re
-
-def sentenceFixer(a):
-    out = ""
-    for ch in a:
-        if ch.isdigit():
-            continue
-        out += ch
-    arr = []
-    arr = re.split('#| $ |%', out)
-    for thing in arr:
-        if thing == "":
-            continue
-        print(thing+".")
-pass
-
-
-sentenceFixer("I wil3l th2ing as234df dfs a sd f#i shoul4d not h5ave s23omething%bla bla bal$")
-
-
-
-t1 = (1,2)
-
-t2 = t1 + (5,6)
-
-t3 =( t1 + t2) * 2
-
-print(len(t3))
-
-t1 = (1,2)
-
-t2 = t1 + (5,6)
-
-t3 = t2 + t1
-
-print(t3.count(1))
+outfile = open("out.txt","w")
+outfile.write("A")
+infile = open("out.txt","r")
+data = infile.read()
+print(len(data))
