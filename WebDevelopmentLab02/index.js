28d437e65d2caa3fbe50a0cc8c422cf2c45f9b5d
Chenry <henrychen0214@gmail.com>
2024-10-28 23:00:35 -0400

    asdf

--- WebDevelopmentLab02/index.js
+++ WebDevelopmentLab02/index.js
@@ -1,35 +1,70 @@
 console.log(document.location.search);
 
-document.addEventListener('DOMContentLoaded', postData)
+document.addEventListener('DOMContentLoaded', postData);
 
 
 const data = document.location.search;
-const params = new URLSearchParams();
+const params = new URLSearchParams(data);
 
-const name = params.get('fname');
-const amount = params.get('amount');
-const genre = params.getAll('genre');
-const medical = params.get('medical');
-const thriller = params.get('thriller')
+console.log(params)
 
-let characters;
+const namee = params.get('fname');
+const age = params.get('age');
+const q1 = params.getAll('question1');
+const q3 = params.getAll('question3');
+const q5 = params.getAll('question5');
+const q7 = params.getAll('question7');
+const q2 = params.get('question2');
+const q4 = params.get('question4');
+const q6 = params.get('question6');
 
-if (medical == "ER" && thriller == "a") {
-    characters = 'er and a'
-}
-else if (medical == "greys" && thriller == "asdf") {
-    characters = 'greys and asdf'
-}
-else if (medical == "chicago-med" && thriller == "b") {
-    characters = 'chicago and b'
-}
-else {
-    characters = 'everything else'
+
+let colorWeights = [0, 0, 0, 0, 0];
+console.log(namee);
+console.log("name");
+console.log(q1);
+console.log("q1");
+function weightsFromArrays(input){
+    console.log(input);
+    input.forEach(element => {
+        weightsFromString(element);
+    });
 }
 
-console.log(characters)
+function weightsFromString(input){
+    console.log(input);
+    if (input == "red"){
+        colorWeights[0] += 1;
+    }
+    if (input == "yellow"){
+        colorWeights[1] += 1;
+    }
+    if (input == "green"){
+        colorWeights[2] += 1;
+    }
+    if (input == "blue"){
+        colorWeights[3] += 1;
+    }
+    if (input == "purple"){
+        colorWeights[4] += 1;
+    }
+}
+
+function updateWeights(){
+    console.log("hello");
+    weightsFromArrays(q1);
+    weightsFromArrays(q3);
+    weightsFromArrays(q5);
+    weightsFromArrays(q7);
+    weightsFromString(q2);
+    weightsFromString(q4);
+    weightsFromString(q6);
+    console.log("hello2");
+}
 
 function postData() {
     const container = document.getElementById("results");
-    container.innerHTML = `<h1>${characters} asfd</h1>`;
+    updateWeights();
+    container.innerHTML = `<h1>${q1} asfd</h1>`;
+    console.log(colorWeights);
 }
