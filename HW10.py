67e2850dd9e80d8da19a212febf01cfeb47d873c
Chen, Po Cheng <pchen432@gatech.edu>
2024-11-15 18:52:10 -0500

    asdf

---
+++ HW10.py
@@ -0,0 +1,84 @@
+"""
+Georgia Institute of Technology - CS1301
+HW10 - OOP
+"""
+
+#########################################
+
+class Pet:
+    def __init__():
+        pass
+
+    def is_adoptable():
+        pass
+
+    def update_happiness():
+        pass
+
+    def feed():
+        pass
+
+    def checkup():
+        pass
+
+    def up_for_adoption():
+        pass
+        
+    def __gt__():
+        pass
+
+    def __str__():
+        pass
+
+#########################################
+
+class Owner:
+
+    def __init__():
+        pass
+
+    def can_afford():
+        pass
+
+    def adopt_pet():
+        pass
+    
+    def __lt__():
+        pass
+
+    def __str__():
+        pass
+
+#########################################
+
+
+class AdoptionCenter:
+
+    def __init__():
+        pass
+
+    def add_pet():
+        pass
+
+    def remove_pet():
+        pass
+    
+    def log_adoption():
+        pass
+    
+    def find_pet_by_species():
+        pass
+
+    def adopted_pets_dict():
+        pass
+
+    def happiness_of_adopted_pets():
+        pass
+
+    def __eq__():
+        pass
+
+    def __str__():
+        pass
+
+                
