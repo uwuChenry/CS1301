aceb2ece61ce3c453861bff3ffbf507b705642a2
Chen, Po Cheng <pchen432@gatech.edu>
2024-11-18 14:05:22 -0500

    lllll

--- HW10.py
+++ HW10.py
@@ -6,16 +6,7 @@ HW10 - OOP
 #########################################
 
 class Pet:
-    name = ""
-    species = ""
-    weight = 0
-    fee = 0
-    age = 0
-    happiness = 0
-    adopted = False
-    isVaccinated = False
-    owner = ""
-    def __init__(self, iname, ispecies, iweight, ifee, iage, ihappiness, iadopted, ivaccinated, iowner):
+    def __init__(self, iname, ispecies, iweight, ifee, iage = 1, ihappiness = 50, iadopted = False, ivaccinated = False, iowner = None):
         self.name = iname
         self.species = ispecies
         self.weight = iweight
@@ -28,7 +19,7 @@ class Pet:
         pass
 
     def is_adoptable(self):
-        if self.adopted or self.happiness >= 10:
+        if self.adopted or self.happiness <= 10:
             return False
         return True
         pass
@@ -40,10 +31,10 @@ class Pet:
         pass
 
     def feed(self, food):
-        if self.happiness == 100:
+        if self.happiness >= 100:
             return f"{self.name} is full."
         if food != self.species:
-            self.happiness -= 20
+            self.happiness -= 30
             if self.happiness <= 20:
                 print(f"{self.name} needs to go to the vet.")
             return f"{self.species}s don't eat {food} food!"
@@ -59,7 +50,13 @@ class Pet:
         self.isVaccinated = True
         pass
 
-    def up_for_adoption():
+    def up_for_adoption(self, owner, afee):
+        if self.adopted:
+            self.adopted = False
+            self.checkup()
+            self.owner = None
+            self.fee = afee
+            owner.pets.remove(self)
         pass
         
     def __gt__(self, pet):
@@ -78,9 +75,6 @@ class Pet:
 #########################################
 
 class Owner:
-    name = ""
-    budget = 0.0
-    pets = []
     def __init__(self, iname, ibudget = 150, ipets = []):
         self.name = iname
         self.budget = ibudget
@@ -115,32 +109,62 @@ class Owner:
 
 
 class AdoptionCenter:
-
-    def __init__():
+    def __init__(self, iname, ipets = [], iowners = [], irevenue = 0):
+        self.name = iname
+        self.pets = ipets
+        self.owners = iowners
+        self.revenue = irevenue
         pass
 
-    def add_pet():
+    def add_pet(self, pet):
+        self.pets.append(pet)
+        return f"{pet.name} has been added to the adoption center."
         pass
 
-    def remove_pet():
+    def remove_pet(self, pet):
+        if pet in self.pets:
+            self.pets.remove(pet)
+        
         pass
     
-    def log_adoption():
+    def log_adoption(self, pet, owner):
+        if not pet.adopted and owner.budget >= pet.fee:
+            self.owners.append(owner)
+            self.revenue += pet.fee
+            owner.adopt_pet(pet)
+            self.remove_pet(pet)
+            return f"{owner.name} has adopted {pet.name}!"
         pass
     
-    def find_pet_by_species():
+    def find_pet_by_species(self, species):
+        temp = []
+        for pets in self.pets:
+            if pets.species == species:
+                temp.append(pets)
+        return temp
         pass
 
-    def adopted_pets_dict():
+    def adopted_pets_dict(self):
+        out = {}
+        for owner in self.owners:
+            out[owner.name] = owner.pets
+        return out
         pass
 
-    def happiness_of_adopted_pets():
+    def happiness_of_adopted_pets(self):
+        s = 0
+        for owner in self.owners:
+            for pets in owner:
+                s += pets.happiness
+        return s
         pass
 
-    def __eq__():
+    def __eq__(self, other):
+        if self.name == other.name and self.revenue == other.revenue:
+            return True
+        return False
         pass
 
-    def __str__():
+    def __str__(self):
+        return f"{self.name} has {len(self.pets)} pets available for adoption."
         pass
-
-                
