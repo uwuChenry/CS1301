ec4ffc5cbab6f7b29f04bbdd5a02c922c0497540
Chenry <henrychen0214@gmail.com>
2024-11-21 12:22:59 -0500

    asfd

--- HW10.py
+++ HW10.py
@@ -16,19 +16,16 @@ class Pet:
         self.adopted = iadopted
         self.isVaccinated = ivaccinated
         self.owner = iowner
-        pass
 
     def is_adoptable(self):
         if self.adopted or self.happiness <= 10:
             return False
         return True
-        pass
 
     def update_happiness(self, boost):
         self.happiness += boost
         if self.happiness > 100:
             self.happiness = 100
-        pass
 
     def feed(self, food):
         if self.happiness >= 100:
@@ -41,14 +38,12 @@ class Pet:
         self.happiness += 20
         self.weight += 2
         return f"{self.name} is well fed."
-        pass
 
     def checkup(self):
         if self.happiness >= 100:
             return f"{self.name}: Happiness - {self.happiness}"
         self.happiness = 100
         self.isVaccinated = True
-        pass
 
     def up_for_adoption(self, owner, afee):
         if self.adopted:
@@ -57,20 +52,17 @@ class Pet:
             self.owner = None
             self.fee = afee
             owner.pets.remove(self)
-        pass
         
     def __gt__(self, pet):
         if self.age > pet.age and self.weight > pet.weight:
             return True
         return False
-        pass
 
     def __str__(self):
         temp = "Available"
         if self.adopted:
             temp = "Adopted"
         return f"{self.name} ({self.species}, {self.age} yrs, {self.happiness} happiness) - {temp}, Fee: ${self.fee}"
-        pass
 
 #########################################
 
@@ -79,13 +71,11 @@ class Owner:
         self.name = iname
         self.budget = ibudget
         self.pets = ipets
-        pass
 
     def can_afford(self, pet):
         if self.budget > pet.fee:
             return True
         return False
-        pass
 
     def adopt_pet(self, pet):
         if self.can_afford(pet):
@@ -94,7 +84,6 @@ class Owner:
             self.pets.append(pet)
             pet.name = self.name
             return f"{pet.name} has been adopted!"
-        pass
     
     def __lt__(self, other):
         if self.budget < other.budget and len(self.pets) < len(other.pets):
@@ -109,23 +98,22 @@ class Owner:
 
 
 class AdoptionCenter:
-    def __init__(self, iname, ipets = [], iowners = [], irevenue = 0):
+    def __init__(self, iname, ipets = None, iowners = None, irevenue = 0):
         self.name = iname
-        self.pets = ipets
-        self.owners = iowners
+        if ipets == None:
+            self.pets = []
+        if iowners == None:
+            self.owners = []
         self.revenue = irevenue
-        pass
 
     def add_pet(self, pet):
         self.pets.append(pet)
         return f"{pet.name} has been added to the adoption center."
-        pass
 
     def remove_pet(self, pet):
         if pet in self.pets:
             self.pets.remove(pet)
         
-        pass
     
     def log_adoption(self, pet, owner):
         if not pet.adopted and owner.budget >= pet.fee:
@@ -134,7 +122,8 @@ class AdoptionCenter:
             owner.adopt_pet(pet)
             self.remove_pet(pet)
             return f"{owner.name} has adopted {pet.name}!"
-        pass
+        return
+
     
     def find_pet_by_species(self, species):
         temp = []
@@ -142,14 +131,14 @@ class AdoptionCenter:
             if pets.species == species:
                 temp.append(pets)
         return temp
-        pass
+
 
     def adopted_pets_dict(self):
         out = {}
         for owner in self.owners:
             out[owner.name] = owner.pets
         return out
-        pass
+
 
     def happiness_of_adopted_pets(self):
         s = 0
@@ -157,14 +146,27 @@ class AdoptionCenter:
             for pets in owner:
                 s += pets.happiness
         return s
-        pass
+
 
     def __eq__(self, other):
         if self.name == other.name and self.revenue == other.revenue:
             return True
         return False
-        pass
+
 
     def __str__(self):
         return f"{self.name} has {len(self.pets)} pets available for adoption."
-        pass
+
+
+
+# Dennys = AdoptionCenter("Dennys")
+# Homeward = AdoptionCenter("Homeward")
+# Homeward = AdoptionCenter("Homeward")
+# Callie = Pet("Callie", "Cat", 8, 350, 10, 10)
+# print(Homeward.add_pet(Callie))
+# Homeward = AdoptionCenter("Homeward")
+# Callie = Pet("Callie", "Cat", 8, 350, 10, 10)
+# Gracie = Pet("Gracie", "Dog", 15, 175, 6, 100)
+# Homeward.add_pet(Callie)
+# print(Homeward.add_pet(Gracie))
+# print(Homeward)
